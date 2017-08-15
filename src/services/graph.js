963471ef0f9dc811b01aeada36c8a16504eb1151
nguyenlevulong <vulongvn98@gmail.com>
2017-08-15 23:39:43 +0700

    Add Graph API

---
+++ src/services/graph.js
@@ -0,0 +1,19 @@
+const graphApiVersion = 'v2.10'
+
+const graph = async (token, resource, params = {}, method = 'GET') => {
+    let url = `https://graph.facebook.com/${graphApiVersion}/${resource}?access_token=${token}`
+    Object.entries(params).forEach(([key, value]) => {
+        url += `&${key}=${value}`
+    })
+    const response = await fetch(url, { method })
+    const json = await response.json()
+    return json.data
+}
+
+export const getGroups = async (token) => graph(token, 'me/groups', { limit: 9999 })
+export const getPostsFromGroup = async (token, groupId) => graph(token, `${groupId}/feed`, { limit: 100 })
+export const getCommentsFromPost = async(token, postId) => graph(token, `${postId}/comments`, { limit: 9999 })
+export const postCommentToPost = async(token, postId, message) => graph(token, `${postId}/comments`, { message }, 'POST')
+
+// Currently it is impossible to publish post to group
+// export const postPostToGroup = async (token, groupId, message) => graph(token, `${groupId}/feed`, { message }, 'POST')
