cfd57b7796a3c3880297d6cf7fdd9a2a5632bef4
Atheros <nguyenvuminh98@gmail.com>
2017-08-23 12:11:26 +0700

    Remove onlick event on div tag Click on PostTime redirect to GroupPost Add CSS for Hashtags

--- src/components/GroupPost.js
+++ src/components/GroupPost.js
@@ -4,29 +4,31 @@ import Linkify from 'react-linkify'
 import * as utils from '../services/utils'
 
 export default ({ post }) =>{
-
-    const id = post.id.split("_")
-    const postLink = "https://facebook.com/groups/" + id[0] + "/permalink/" + id[1];
+    const postLink = "https://facebook.com/" + post.id
     const profileLink = "https://facebook.com/" + post.from.id;
 
     const redirect = (address) => {    
         window.open(address);
     }
     return(
-    <div className = {styles.container} onClick = {() => redirect(postLink)}>
+    <div className = {styles.container}>
         <div>
             <img className = {styles.avatar} src = {post.from.avatar} alt = {post.from.name + "'s avatar"}/>
-            <p className = {styles.name} onClick = {(event) => {
-                event.stopPropagation();redirect(profileLink)}
-            }>{post.from.name}</p>
-            <p id = {styles.time}>{utils.getDateInVietnamese(post.created_time)}</p>
+            <p className = {styles.name} onClick = {() => {redirect(profileLink)}}>
+                {post.from.name}
+            </p>
+            <p onClick = {() => redirect(postLink)} id = {styles.time}>
+                {utils.getDateInVietnamese(post.created_time)}
+            </p>
         </div>
         <div className={styles.post}>
-            <Linkify properties = {{target: '_blank', onClick: (event) => event.stopPropagation(), style:{color: '#365899'} }}>
+            <Linkify properties = {{target: '_blank', style:{color: '#365899'} }}>
             {post.message}
             </Linkify>
-            {utils.getHashtags(post.message).length !== 0 ? <p>{utils.getHashtags(post.message).map(values =>
-                values)}</p> : ""}
+            {utils.getHashtags(post.message).length !== 0 ? 
+            <div>{utils.getHashtags(post.message).map(values =>
+                <div className={styles.hashtag}>{values}</div>)}
+            </div> : ""}
         </div>
     </div>
 )}
