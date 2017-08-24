d03d0cdd72a5fb9cd868227fdd27e9428a33b3c4
baymax98 <nguyenquanglinh98@gmail.com>
2017-08-25 02:14:04 +0700

    Add files via upload

--- src/components/GroupListItem.js
+++ src/components/GroupListItem.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import styles from './GroupListItem.css'
+
 export default ({ group, onSelect }) => (
     <div>
-    <div onClick={() => onSelect(group.id)}>
+    <div className={styles.items}
+    onClick={() => onSelect(group.id)}>
         <p>{group.name}</p>
     </div>
     <button> remove </button>
