6aab415e9c0b192ce8e686af9672a4741a4fe15b
nguyenlevulong <vulongvn98@gmail.com>
2017-09-09 22:03:10 +0700

    Done Loading component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,3 +1,18 @@
 import React from 'react'
+import CircularProgress from 'material-ui/CircularProgress'
 
-export default () => <p>Loading......</p>
+const styles = {
+    flex: '1 1 auto',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+}
+
+export default () => (
+    <div style={styles}>
+        <CircularProgress
+            size={100}
+            thickness={10}
+        />
+    </div>
+)
