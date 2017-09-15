681e2ec76d73484750c223e24923bc763af9810b
nguyenlevulong <vulongvn98@gmail.com>
2017-09-15 08:02:42 +0700

    GroupFeed scroll to top when change group

--- src/containers/GroupFeed.js
+++ src/containers/GroupFeed.js
@@ -1,13 +1,26 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import GroupPost from '../components/GroupPost'
 import styles from './GroupFeed.css'
 
-export default ({ group }) => {
-    return (
-        <div className={styles.container}>
-            {group.feed.map(post => 
-                <GroupPost key={post.id} post={post} admins={group.admins} />
-            )}
-        </div>
-    )
+export default class GroupFeed extends React.Component {
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.group.id !== this.props.group.id) 
+            this.scrollToTop()
+    }
+    scrollToTop = () => {
+        const element = ReactDOM.findDOMNode(this.topMostNodeToScrollInto)
+        element.scrollIntoView()
+    }
+    render() {
+        const { group } = this.props
+        return (
+            <div className={styles.container}>
+                <div ref={node => this.topMostNodeToScrollInto = node}></div>
+                {group.feed.map(post => 
+                    <GroupPost key={post.id} post={post} admins={group.admins} />
+                )}
+            </div>
+        )
+    }
 }
