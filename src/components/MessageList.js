a13283bff50cf032ca698976e29f8d61f4ea62c9
Nguyen Le Vu Long <vulongvn98@gmail.com>
2018-03-30 20:26:35 +0900

    Refactor format

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,38 +1,33 @@
 import React from 'react'
 import Message from './Message'
 class MessageList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      messages: []
+    }
+    this.getData = this.getData.bind(this)
+    this.props.db.getMessage(this.getData)
+  }
 
-    constructor(props){
-        super(props)
-        this.state = {
-            messages: []
-        }
-        this.getData = this.getData.bind(this)
-        this.props.db.getMessage(this.getData)
+  getData(values) {
+    if (values != null) {
+      const messages = Object.entries(values).map(values => {
+        return values[1]
+      })
+      this.setState({ messages: messages })
     }
-
-    getData(values){
-        if (values != null){
-            const messages = Object.entries(values).map(values => {
-                return values[1]
-            })
-            this.setState({messages : messages})
-        }
-    }
-    render(){
-        const messageNodes = this.state.messages.map((message) => {
-            return (
-              <div>
-                  <Message message = {message.id + ': ' + message.message} />
-              </div>
-            )
-          });
-          return (
-            <div>
-              {messageNodes}
-            </div>
-          );
-        }
-    }
+  }
+  render() {
+    const messageNodes = this.state.messages.map(message => {
+      return (
+        <div>
+          <Message message={message.id + ': ' + message.message} />
+        </div>
+      )
+    })
+    return <div>{messageNodes}</div>
+  }
+}
 
 export default MessageList
